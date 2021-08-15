ac13ff3c6b749c2e67de3342c73920aa442a1a9f
George Dilthey <george.dilthey@gmail.com>
2021-08-14 20:38:04 -0400

    Get events from API

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,21 @@
 class Event < ApplicationRecord
     has_many :commits
 
-    def event_from_api
+    def self.get_events
         url = 'https://api.github.com/users/george-dilthey/events/public'
+        response = HTTParty.get(url)
+        result = response.parsed_response
+        result.each {|e| create_from_api(e)}
+        
     end
+
+    def self.create_from_api(event)
+        new_event = find_or_create_by(id: event["id"]) do |e|
+            e.id = event["id"]
+            e.type = event["type"]
+            e.repo_id = event["repo"]["id"]
+            e.timestamp = event["created_at"]
+        end 
+    end
+    
 end
